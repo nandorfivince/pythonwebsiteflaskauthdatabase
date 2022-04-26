6a042bfc28200c8d3f1b0c1b66d24afc1484e676
Vince Nándorfi <vince.nandorfi@affinitext.com>
2022-04-26 10:51:45 +0200

    templating

--- website/__init__.py
+++ website/__init__.py
@@ -2,12 +2,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'blabla'
-
-    from .views import views
-    from .auth import auth
-
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.config['SECRET_KEY'] = 'blablabla bla'
 
     return app
+
+    
